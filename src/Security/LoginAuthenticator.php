7cd58b3c6b338ef699f8eb87867125eabebca90a
aperio2020 <audrey.perio@hotmail.fr>
2021-10-19 17:23:06 +0200

    commit creation

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -53,7 +53,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('connexion'));
+        return new RedirectResponse($this->urlGenerator->generate('sorties'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
