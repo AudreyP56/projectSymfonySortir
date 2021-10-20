28c54b80d0a6e4ebdee2bbb3602f2559dba3528b
Quentin742 <olivier49530@hotmail.fr>
2021-10-20 17:24:35 +0200

    Push de tentative de changement de la navbar pour la rendre responsive mais non fonctionnel (je pense que le problème se trouve dans le script). Il y a aussi l'avancement du controller d'annulation d'une sortie

--- src/Controller/AnnulationSortieController.php
+++ src/Controller/AnnulationSortieController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Site;
 use App\Entity\Sortie;
-use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,27 +14,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class AnnulationSortieController extends AbstractController
 {
     #[Route('/sorties/annulation/sortie/{id}', name: 'annulation_sortie')]
-    public function index($id, EntityManagerInterface $entityManager): Response
+    public function index($id, EntityManagerInterface $entityManager, Request $request ): Response
     {
-
-
         //J'ai besoin de récupérer un ville. Sauf que la sortie ne pointe pas sur une ville mais si un lieu qui pointe sur une ville
         //Donc il me faut récupérer le lieu puis ensuite je peux récupérer la ville
         $currentSortie = $entityManager->getRepository(Sortie::class)->find($id);
 
         $lieu = $currentSortie->getLieuId();
 
-        if($currentSortie->getLieuId()->getNom() == null)
-        {
-            return new Response("C'est nul");
-        }
-
         $ville = $lieu->getVilleId();
-
 
         $annulationSortieForm = $this->createFormBuilder()
             ->add('motif', TextareaType::class, ['label' => 'Motif :'])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
+            ->add('reset', ResetType::class, ['label' => 'Annuler'])
             ->getForm();
+
+
+        $annulationSortieForm->handleRequest($request);
+        if($annulationSortieForm->isSubmitted())
+        {
+            $data = $annulationSortieForm->getData();
+            #Il faudra modifier l'état de la sortie (la passé en annuler)
+            #Il faudra aussi trouver ou stocker le motif d'annulation
+            $currentSortie->setEtat();
+        }
+
 
 
         return $this->render('annulation_sortie/index.html.twig', [
