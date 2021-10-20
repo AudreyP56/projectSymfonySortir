e28239d4b8ecc6a52ec68e080a3187fccf506dcc
murvoy2020 <maxime.ptitx@gmail.com>
2021-10-20 11:29:48 +0200

    affiche l'action inscription/desinscription en fonction de l'etat effectif de l'inscription/desinscription de l'utilisateur

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -6,6 +6,7 @@ use App\Repository\SortieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Boolean;
 
 /**
  * @ORM\Entity(repositoryClass=SortieRepository::class)
@@ -216,5 +217,16 @@ class Sortie
         $this->participants->removeElement($participant);
 
         return $this;
+    }
+
+    public function isParticipant(User $user): Bool
+    {
+        $participants = $this->participants;
+        $isParticipant = false;
+
+        if ( $participants->contains($user) ) {
+            $isParticipant = true;
+        }
+        return  $isParticipant;
     }
 }
