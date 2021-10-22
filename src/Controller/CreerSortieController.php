5ad68259bd3f0ddfeabae723a492f12b4d4170b2
aperio2020 <audrey.perio@hotmail.fr>
2021-10-22 14:58:51 +0200

    page modification sortie

--- src/Controller/CreerSortieController.php
+++ src/Controller/CreerSortieController.php
@@ -8,16 +8,14 @@ use App\Entity\Site;
 use App\Entity\Sortie;
 use App\Entity\User;
 use App\Entity\Ville;
-use App\Form\CreationSortieType;
 use App\Repository\LieuRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -56,7 +54,7 @@ class CreerSortieController extends AbstractController
             ->add('dateLimite', DateType::class)
             ->add('nbPlace', NumberType::class)
             ->add('duree', NumberType::class)
-            ->add('description', TextType::class)
+            ->add('description', TextareaType::class)
             ->add('lieu', TextType::class)
             ->add('rue', TextType::class)//$rue getRue() en fonction du lieu selectionné
             ->add('codePostal', TextType::class)//$codePostal getCodePostal()
@@ -105,7 +103,85 @@ class CreerSortieController extends AbstractController
         ]);
     }
 
-       /**
+    #[Route('/sortie/modifier/{id}', name: 'modifier_sortie')]
+    public function modification(Request $request, EntityManagerInterface $entityManager, $id): Response{
+
+        //user courant
+        $user = $this->getUser();
+        $userBase = $entityManager->getRepository(User::class)->find($user->getId());
+        //repos
+
+        $repoSite = $this->getDoctrine()->getRepository(Site::class);
+        $repoVille = $this->getDoctrine()->getRepository(Ville::class);
+        $repoLieu = $this->getDoctrine()->getRepository(Lieu::class);
+        $repoEtat = $this->getDoctrine()->getRepository(Etat::class);
+        $sortie = $entityManager->getRepository(Sortie::class)->find($id);
+        $lieu = $repoLieu->find($sortie->getLieuId());
+
+        $site = $repoSite->find($user->getSiteId());
+        $villes = $repoVille->findAll();
+
+        $creationForm = $this->createFormBuilder()
+            ->add('nom', TextType::class, ['attr'=> ['value'=>$sortie->getNom()]])
+            ->add('dateHeureSortie', DateTimeType::class, ['data'=>$sortie->getDateHeureSortie()])
+            ->add('dateLimite', DateType::class, ['data'=>$sortie->getDateLimite()])
+            ->add('nbPlace', NumberType::class, ['attr'=>['value'=>$sortie->getNbPlace()]])
+            ->add('duree', NumberType::class, ['attr'=>['value'=>$sortie->getDuree()]])
+            ->add('description', TextareaType::class, ['data'=>$sortie->getDescription()])
+            ->add('lieu', TextType::class, ['attr'=>['value'=>$sortie->getLieuId()]])
+            ->add('rue', TextType::class, ['attr'=>['value'=>$lieu->getRue()]])
+            ->add('codePostal', TextType::class, ['attr'=>['value'=>$lieu->getVille()->getCodePostal()]])
+            ->add('latitude', TextType::class, ['attr'=>['value'=>$lieu->getLatitude()]])
+            ->add('longitude', TextType::class, ['attr'=>['value'=>$lieu->getLongitude()]])
+            ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
+            ->add('saveEtPublier', SubmitType::class, ['label' => 'Publier'])
+            ->add('supprimer', SubmitType::class,['label'=>'Supprimer'])
+            ->getForm();
+        $creationForm->handleRequest($request);
+
+
+
+        if($creationForm->isSubmitted() && $creationForm->isValid()){
+            $data = $creationForm->getData();
+
+            $lieuSelect = $repoLieu->findBy(["nom" => $data["lieu"]]);
+            $lieu = $lieuSelect[0];
+            $sortie->setOrganisateur($userBase);
+            $sortie->setNom($data["nom"]);
+            $sortie->setDateHeureSortie($data["dateHeureSortie"]);
+            $sortie->setDateLimite($data["dateLimite"]);
+            $sortie->setNbPlace($data["nbPlace"]);
+            $sortie->setDuree($data["duree"]);
+            $sortie->setDescription($data["description"]);
+            $sortie->setLieuId($lieu);// a verifier
+
+            if ($creationForm->get('save')->isClicked() ) {
+                $etatCreation = $repoEtat->findBy(['label'=> Etat::STATUS_EN_CREATION]);
+                $sortie->setEtat($etatCreation[0]);
+            }
+            if ($creationForm->get('saveEtPublier')->isClicked() ) {
+                $etatOuverte = $repoEtat->findBy(['label'=> Etat::STATUS_OUVERTE]);
+                $sortie->setEtat( $etatOuverte[0]);
+            }
+            if($creationForm->get('supprimer')->isClicked()){
+                $entityManager->remove($sortie);
+                $entityManager->flush($sortie);
+                return $this->redirectToRoute('sorties');
+            }
+            $entityManager->flush();
+
+            return $this->redirectToRoute('sorties');
+        }
+        return $this->render('creer_sortie/index.html.twig', [
+            'controller_name' => 'CreerSortieController',
+            'creationForm' => $creationForm->createView(),
+            'villes' => $villes,
+            'site' => $site,
+            'lieu' => $lieu,
+        ]);
+    }
+
+    /**
      * @Route("listeLieu/{id}", name="listeLieu")
      */
     public function listeLieu(Ville $ville, LieuRepository $lieuRepository){
