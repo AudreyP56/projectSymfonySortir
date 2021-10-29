f53be3735076c12d0cba7b582e08ff4a6cc037a1
aperio2020 <audrey.perio@hotmail.fr>
2021-10-29 10:10:44 +0200

    corrections sonar

--- src/Controller/CreerSortieController.php
+++ src/Controller/CreerSortieController.php
@@ -17,7 +17,6 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -63,8 +62,6 @@ class CreerSortieController extends AbstractController
         if($creationForm->isSubmitted()){
 
             $data = $creationForm->getData();
-
-            $lieu = $repoLieu->findBy(["nom" => $data["lieu"]]);
 
             if($data["dateLimite"] <= $data["dateHeureSortie"]){
                 if(($data["lieu"] !== "0") && ($data["lieu"] !== "-- Sélectionner un lieu --") ) {
