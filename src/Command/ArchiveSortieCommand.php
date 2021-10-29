37c0c1d1b007f31556d85836c24bda42c89fb90f
murvoy2020 <maxime.ptitx@gmail.com>
2021-10-29 09:55:58 +0200

    Revert "suppression commentaire essentiellement"
    This reverts commit 4efa8fa2ccd71f058ff298057c77914988e6008b.

--- src/Command/ArchiveSortieCommand.php
+++ src/Command/ArchiveSortieCommand.php
@@ -26,6 +26,11 @@ class ArchiveSortieCommand extends Command
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $today = new \DateTime("now");
--- src/Repository/ResetLinkRepository.php
+++ src/Repository/ResetLinkRepository.php
@@ -18,4 +18,33 @@ class ResetLinkRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ResetLink::class);
     }
+
+    // /**
+    //  * @return ResetLink[] Returns an array of ResetLink objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('r.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?ResetLink
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
 }
